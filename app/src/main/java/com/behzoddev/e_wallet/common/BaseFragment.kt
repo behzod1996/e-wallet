16aebdfd84edd80d4ab82ede403c3dd7193318a0
Behzod Bozorboev <behzoddev@gmail.com>
2022-01-08 22:32:19 +0900

    Add base fragment class

---
+++ app/src/main/java/com/behzoddev/e_wallet/common/BaseFragment.kt
@@ -0,0 +1,25 @@
+package com.behzoddev.e_wallet.common
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment <VB: ViewBinding> : Fragment() {
+
+    private var _binding: VB? = null
+    protected val binding = _binding!!
+
+    abstract fun initializeBinding(inflater: LayoutInflater,container: ViewGroup?): VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = initializeBinding(inflater,container)
+        return binding.root
+    }
+}
