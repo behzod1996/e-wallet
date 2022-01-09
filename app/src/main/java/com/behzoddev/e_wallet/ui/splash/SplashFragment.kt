f9fa5c32e64b4f240d328f61d78c40c81d72482b
Behzod Bozorboev <behzoddev@gmail.com>
2022-01-09 15:36:53 +0900

    Update UI

--- app/src/main/java/com/behzoddev/e_wallet/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/behzoddev/e_wallet/ui/splash/SplashFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.behzoddev.e_wallet.R
 import com.behzoddev.e_wallet.common.BaseFragment
 import com.behzoddev.e_wallet.databinding.FragmentSplashBinding
 
@@ -18,5 +20,12 @@ class SplashFragment : BaseFragment<FragmentSplashBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navigateToDashboard()
+    }
+
+    private fun navigateToDashboard() = with(binding) {
+        btnStart.setOnClickListener {
+            findNavController().navigate(R.id.actionFromSplashToDashboard)
+        }
     }
 }
