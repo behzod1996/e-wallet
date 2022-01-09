488196a1196b47f14b5b445823277ad64dbbb5bc
Behzod Bozorboev <behzoddev@gmail.com>
2022-01-09 19:57:18 +0900

    Add transaction data class

---
+++ app/src/main/java/com/behzoddev/e_wallet/data/local/Transaction.kt
@@ -0,0 +1,26 @@
+package com.behzoddev.e_wallet.data.local
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = RoomContract.Transaction.TRANSACTION_TABLE_NAME)
+data class Transaction(
+    @ColumnInfo(name = RoomContract.Transaction.TRANSACTION_TITLE)
+    var transactionTitle: String,
+    @ColumnInfo(name = RoomContract.Transaction.TRANSACTION_AMOUNT)
+    var transactionAmount: String,
+    @ColumnInfo(name = RoomContract.Transaction.TRANSACTION_TYPE)
+    var transactionType: String,
+    @ColumnInfo(name = RoomContract.Transaction.TRANSACTION_TAG)
+    var transactionTag: String,
+    @ColumnInfo(name = RoomContract.Transaction.TRANSACTION_DATE)
+    var transactionDate: String,
+    @ColumnInfo(name = RoomContract.Transaction.TRANSACTION_DESC)
+    var transactionDesc: String,
+    @ColumnInfo(name = RoomContract.Transaction.TRANSACTION_CREATE_DATE)
+    var transactionCreateDate: Long = System.currentTimeMillis(),
+    @ColumnInfo(name = RoomContract.Transaction.TRANSACTION_TITLE)
+    @PrimaryKey(autoGenerate = true)
+    var transactionId: Int = 0
+)
