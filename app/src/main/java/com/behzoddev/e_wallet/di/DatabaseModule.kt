168d5f70c159ebf163222bc71637ea5d9de8a90c
Behzod Bozorboev <behzoddev@gmail.com>
2022-01-10 20:41:12 +0900

    Add database module

---
+++ app/src/main/java/com/behzoddev/e_wallet/di/DatabaseModule.kt
@@ -0,0 +1,29 @@
+package com.behzoddev.e_wallet.di
+
+import android.app.Application
+import com.behzoddev.e_wallet.data.local.Transaction
+import com.behzoddev.e_wallet.data.local.TransactionDao
+import com.behzoddev.e_wallet.data.local.TransactionDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideRoomDatabase(application: Application): TransactionDatabase {
+        return TransactionDatabase.invoke(application.applicationContext)
+    }
+
+    @Provides
+    @Singleton
+    fun provideDaoInstance(transactionDatabase: TransactionDatabase): TransactionDao {
+        return transactionDatabase.getDao()
+    }
+
+}
