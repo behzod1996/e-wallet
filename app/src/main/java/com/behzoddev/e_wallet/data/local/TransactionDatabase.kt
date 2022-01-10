80c7e66d0f3e5b5255943573bc6db8cfb71c1215
Behzod Bozorboev <behzoddev@gmail.com>
2022-01-10 20:40:13 +0900

    Add database class

---
+++ app/src/main/java/com/behzoddev/e_wallet/data/local/TransactionDatabase.kt
@@ -0,0 +1,32 @@
+package com.behzoddev.e_wallet.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Transaction::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class TransactionDatabase : RoomDatabase() {
+
+    abstract fun getDao(): TransactionDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TransactionDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = INSTANCE ?: synchronized(LOCK) {
+            INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            TransactionDatabase::class.java,
+            RoomContract.Database.DATABASE_TABLE_NAME
+        ).build()
+    }
+}
