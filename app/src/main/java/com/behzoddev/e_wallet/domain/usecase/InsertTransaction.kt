5ec83060b1f165692a99d178ffe3a9d3c7ac31fd
Behzod Bozorboev <behzoddev@gmail.com>
2022-01-20 20:20:37 +0900

    Add use case classes

--- app/src/main/java/com/behzoddev/e_wallet/domain/usecase/InsertTransaction.kt
+++ app/src/main/java/com/behzoddev/e_wallet/domain/usecase/InsertTransaction.kt
@@ -1,12 +1,12 @@
-package com.behzoddev.e_wallet.domain
+package com.behzoddev.e_wallet.domain.usecase
 
 import com.behzoddev.e_wallet.data.local.Transaction
 import com.behzoddev.e_wallet.data.repository.LocalRepository
-import com.behzoddev.e_wallet.di.BindModule
+import com.behzoddev.e_wallet.di.BindModule.RepositoryBinds
 import javax.inject.Inject
 
-class InsertTransactionUseCase @Inject constructor
-    (@BindModule.RepositoryBinds private val repository: LocalRepository)
+class InsertTransaction @Inject constructor
+    (@RepositoryBinds private val repository: LocalRepository)
 {
         suspend fun insertTransaction(transaction: Transaction): Long {
             return repository.insertTransaction(transaction)
