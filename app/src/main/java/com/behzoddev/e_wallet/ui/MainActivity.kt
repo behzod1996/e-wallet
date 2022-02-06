7db526fb79e8e0f6ae320c460f16c7c95d47935e
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-06 12:17:43 +0900

    Update main activity

---
+++ app/src/main/java/com/behzoddev/e_wallet/ui/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.behzoddev.e_wallet.ui
+
+import com.behzoddev.e_wallet.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navHostFragment: NavHostFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initializeNavigation()
+    }
+
+    private fun initializeNavigation() {
+        navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host) as NavHostFragment?
+            ?: return
+    }
+}
