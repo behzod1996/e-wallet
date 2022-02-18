e57b353b3a92dfb9d8ab06e887491ac1800485cb
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-18 19:54:40 +0900

    Update AddFragment.kt and AddViewModel.kt

--- app/src/main/java/com/behzoddev/e_wallet/presentation/add/AddViewModel.kt
+++ app/src/main/java/com/behzoddev/e_wallet/presentation/add/AddViewModel.kt
@@ -3,20 +3,22 @@ package com.behzoddev.e_wallet.presentation.add
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.behzoddev.e_wallet.data.local.TransactionModel
+import com.behzoddev.e_wallet.domain.interactor.InsertTransactionInteractor
+import com.behzoddev.e_wallet.domain.interactor.UpdateTransactionInteractor
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class AddViewModel @Inject constructor(
-    private val insertTransaction: InsertTransactionUseCase,
-    private val updateTransaction: UpdateTransactionUseCase
+    private val insertTransaction: InsertTransactionInteractor,
+    private val updateTransaction: UpdateTransactionInteractor
 )  : ViewModel() {
 
     fun insertTransaction(transactionModel: TransactionModel) = viewModelScope.launch {
-        insertTransaction.insertTransaction(transactionModel)
+        insertTransaction.invoke(transactionModel)
     }
     fun updateTransaction(transactionModel: TransactionModel) = viewModelScope.launch {
-        updateTransaction.updateTransaction(transactionModel)
+        updateTransaction.invoke(transactionModel)
     }
 }
--- app/src/main/java/com/behzoddev/e_wallet/ui/add/AddFragment.kt
+++ app/src/main/java/com/behzoddev/e_wallet/ui/add/AddFragment.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.behzoddev.e_wallet.R
 import com.behzoddev.e_wallet.common.base.BaseFragment
+import com.behzoddev.e_wallet.common.extensions.setDatePicker
 import com.behzoddev.e_wallet.databinding.FragmentAddBinding
+import com.behzoddev.e_wallet.utils.constants.TransactionTags
+import com.behzoddev.e_wallet.utils.constants.TransactionType
+import java.util.*
 
 class AddFragment : BaseFragment<FragmentAddBinding>() {
 
@@ -18,5 +24,35 @@ class AddFragment : BaseFragment<FragmentAddBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initializeTag()
+        initializeType()
+        initializeDatePicker()
+    }
+
+
+    private fun initializeTag() = with(binding) {
+        val transactionTag = ArrayAdapter(
+            requireContext(),
+            R.layout.layout_autocomplete,
+            TransactionTags.tags
+        )
+        layoutAdd.actTag.setAdapter(transactionTag)
+    }
+
+    private fun initializeType() = with(binding) {
+        val transactionType = ArrayAdapter(
+            requireContext(),
+            R.layout.layout_autocomplete,
+            TransactionType.types
+        )
+        layoutAdd.actType.setAdapter(transactionType)
+    }
+
+    private fun initializeDatePicker() = with(binding) {
+        layoutAdd.tieDate.setDatePicker(
+            requireContext(),
+            "dd/MM/E",
+            Date()
+        )
     }
 }
