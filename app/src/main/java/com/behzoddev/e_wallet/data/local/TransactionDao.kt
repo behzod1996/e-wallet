3858fbbbedf98f68d108de20fdf65d18d693b0b0
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-18 16:20:53 +0900

    Refactor transaction DAO

--- app/src/main/java/com/behzoddev/e_wallet/data/local/TransactionDao.kt
+++ app/src/main/java/com/behzoddev/e_wallet/data/local/TransactionDao.kt
@@ -7,17 +7,17 @@ import kotlinx.coroutines.flow.Flow
 interface TransactionDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertTransaction(transaction: Transaction): Long
+    suspend fun insertTransaction(transactionModel: TransactionModel): Long
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun updateTransaction(transaction: Transaction): Long
+    suspend fun updateTransaction(transactionModel: TransactionModel): Int
 
     @Delete
-    suspend fun deleteTransaction(transaction: Transaction): Int
+    suspend fun deleteTransaction(transactionModel: TransactionModel): Int
 
     @Query("SELECT * FROM transaction_table ORDER BY transaction_create_date")
-    fun getAllTransactions(): Flow<List<Transaction>>
+    fun getAllTransactions(): Flow<List<TransactionModel>>
 
-    @Query("SELECT * FROM transaction_table WHERE transaction_id =:productId")
-    fun getById(productId: Int): Flow<Transaction>
+    @Query("SELECT * FROM transaction_table WHERE transaction_id = :transactionId")
+    fun getById(transactionId: Int): Flow<TransactionModel>
 }
