c6a811f2249745fb225b3a8a289d0fd92823560e
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-18 16:20:06 +0900

    Update local state

--- app/src/main/java/com/behzoddev/e_wallet/utils/LocalState.kt
+++ app/src/main/java/com/behzoddev/e_wallet/utils/LocalState.kt
@@ -1,10 +1,10 @@
 package com.behzoddev.e_wallet.utils
 
-import com.behzoddev.e_wallet.data.local.Transaction
+import com.behzoddev.e_wallet.data.local.TransactionModel
 
 sealed class LocalState {
     object Loading : LocalState()
     object Empty : LocalState()
-    data class Success(val data: List<Transaction>): LocalState()
+    data class Success(val data: List<TransactionModel>): LocalState()
     data class Error(val message: String) : LocalState()
 }
