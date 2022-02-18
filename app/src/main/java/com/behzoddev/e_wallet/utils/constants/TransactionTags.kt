1a6a240159a2d562fddc359356e99319b519e8ae
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-18 19:53:42 +0900

    Add constants

---
+++ app/src/main/java/com/behzoddev/e_wallet/utils/constants/TransactionTags.kt
@@ -0,0 +1,16 @@
+package com.behzoddev.e_wallet.utils.constants
+
+object TransactionTags {
+    val tags = listOf(
+        "Housing",
+        "Transportation",
+        "Food",
+        "Utilities",
+        "Insurance",
+        "Healthcare",
+        "Saving & Debts",
+        "Personal Spending",
+        "Entertainment",
+        "Miscellaneous"
+    )
+}
---
+++ app/src/main/java/com/behzoddev/e_wallet/utils/constants/TransactionType.kt
@@ -0,0 +1,7 @@
+package com.behzoddev.e_wallet.utils.constants
+
+object TransactionType {
+    val types = listOf(
+        "Income","Expense"
+    )
+}
