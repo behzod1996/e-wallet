96283f724464c1a23240f398cbec9da784e92026
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-18 16:34:41 +0900

    Update WalletViewModel.kt

--- app/src/main/java/com/behzoddev/e_wallet/presentation/wallet/WalletViewModel.kt
+++ app/src/main/java/com/behzoddev/e_wallet/presentation/wallet/WalletViewModel.kt
@@ -1,4 +1,4 @@
-package com.behzoddev.presentation.wallet
+package com.behzoddev.e_wallet.presentation.wallet
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -6,6 +6,7 @@ import com.behzoddev.e_wallet.data.local.TransactionModel
 import com.behzoddev.e_wallet.domain.interactor.DeleteTransactionInteractor
 import com.behzoddev.e_wallet.domain.interactor.FetchAllTransactionsInteractor
 import com.behzoddev.e_wallet.utils.LocalState
+import com.behzoddev.e_wallet.utils.states.TransactionViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -20,16 +21,16 @@ class WalletViewModel @Inject constructor (
     private val deleteTransaction: DeleteTransactionInteractor
         ) : ViewModel() {
 
-    private var _dashboardState = MutableStateFlow<LocalState>(LocalState.Loading)
-    val dashboardState : StateFlow<LocalState> = _dashboardState
+    private var _dashboardState = MutableStateFlow<TransactionViewState>(TransactionViewState.Loading)
+    val dashboardState : StateFlow<TransactionViewState> = _dashboardState
 
     init {
         viewModelScope.launch {
             getAllTransactions.invoke().collect { result ->
                 if (result.isNullOrEmpty()) {
-                    _dashboardState.value = LocalState.Empty
+                    _dashboardState.value = TransactionViewState.Empty
                 } else {
-                    _dashboardState.value = LocalState.Success(result)
+                    _dashboardState.value = TransactionViewState.Success(result)
                 }
             }
         }
