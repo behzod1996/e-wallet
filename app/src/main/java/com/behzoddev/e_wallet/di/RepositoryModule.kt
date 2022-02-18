e46c64a8b86b0da9c77b22f5325c6465ac18acf5
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-18 16:21:41 +0900

    Add repository module

---
+++ app/src/main/java/com/behzoddev/e_wallet/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.behzoddev.e_wallet.di
+
+import com.behzoddev.e_wallet.data.repository.TransactionRepositoryImpl
+import com.behzoddev.e_wallet.domain.repository.TransactionRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun providesRepositoryInstance(
+        transactionRepositoryImpl: TransactionRepositoryImpl
+    ) : TransactionRepository
+}
