f05c94da8a087b1a8855b46c0183b4b5f8641024
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-18 16:23:25 +0900

    Refactor insert interactor

---
+++ app/src/main/java/com/behzoddev/e_wallet/domain/interactor/InsertTransactionInteractorImpl.kt
@@ -0,0 +1,9 @@
+package com.behzoddev.e_wallet.domain.interactor
+
+import com.behzoddev.e_wallet.data.local.TransactionModel
+
+class InsertTransactionInteractorImpl : InsertTransactionInteractor {
+    override suspend fun invoke(transactionModel: TransactionModel): Long {
+        TODO("Not yet implemented")
+    }
+}
