bc0777f00edafe45bb5778538dc7cfcc4d9a7c25
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-19 18:41:06 +0900

    Update interactors

--- app/src/main/java/com/behzoddev/e_wallet/domain/interactor/FetchAllTransactionsInteractorImpl.kt
+++ app/src/main/java/com/behzoddev/e_wallet/domain/interactor/FetchAllTransactionsInteractorImpl.kt
@@ -1,14 +1,17 @@
 package com.behzoddev.e_wallet.domain.interactor
 
+import com.behzoddev.e_wallet.common.coroutine.DispatcherProvider
 import com.behzoddev.e_wallet.data.local.TransactionModel
 import com.behzoddev.e_wallet.domain.repository.TransactionRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class FetchAllTransactionsInteractorImpl @Inject constructor(
-    private val transactionRepository: TransactionRepository
+    private val transactionRepository: TransactionRepository,
+    private val dispatcher: DispatcherProvider
 ) : FetchAllTransactionsInteractor {
     override fun invoke(): Flow<List<TransactionModel>> {
-        return transactionRepository.getAllTransactions()
+        return transactionRepository.getAllTransactions().flowOn(dispatcher.io)
     }
 }
--- app/src/main/java/com/behzoddev/e_wallet/domain/interactor/FetchByIdInteractorImpl.kt
+++ app/src/main/java/com/behzoddev/e_wallet/domain/interactor/FetchByIdInteractorImpl.kt
@@ -1,14 +1,17 @@
 package com.behzoddev.e_wallet.domain.interactor
 
+import com.behzoddev.e_wallet.common.coroutine.DispatcherProvider
 import com.behzoddev.e_wallet.data.local.TransactionModel
 import com.behzoddev.e_wallet.domain.repository.TransactionRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class FetchByIdInteractorImpl @Inject constructor(
-    private val transactionRepository: TransactionRepository
+    private val transactionRepository: TransactionRepository,
+    private val dispatcher: DispatcherProvider
 ) : FetchByIdInteractor {
     override fun invoke(transactionId: Int): Flow<TransactionModel> {
-        return transactionRepository.getById(transactionId)
+        return transactionRepository.getById(transactionId).flowOn(dispatcher.io)
     }
 }
