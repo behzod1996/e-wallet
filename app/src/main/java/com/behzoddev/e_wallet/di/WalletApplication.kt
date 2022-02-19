c1f3ab3aa8971466e62e34e05336fdf85fed4064
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-19 18:40:04 +0900

    Update application class

--- app/src/main/java/com/behzoddev/e_wallet/di/WalletApplication.kt
+++ app/src/main/java/com/behzoddev/e_wallet/di/WalletApplication.kt
@@ -1,8 +1,34 @@
 package com.behzoddev.e_wallet.di
 
 import android.app.Application
+import com.behzoddev.e_wallet.common.extensions.debug
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.FormatStrategy
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class WalletApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val formatStrategy: FormatStrategy = PrettyFormatStrategy.newBuilder()
+            .showThreadInfo(true)
+            .methodCount(1)
+            .methodOffset(5)
+            .tag("")
+            .build()
+
+        Logger.addLogAdapter(AndroidLogAdapter(formatStrategy))
+
+        Timber.plant(object : Timber.DebugTree() {
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                Logger.log(priority,"-$tag",message,t)
+            }
+        })
+        debug { "onCreate() is created" }
+    }
 }
