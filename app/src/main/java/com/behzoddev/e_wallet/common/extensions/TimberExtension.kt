6d736b18502f56a61a0c11377bb7e3c8cdfc97ae
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-19 18:29:09 +0900

    Add TimberExtension.kt

---
+++ app/src/main/java/com/behzoddev/e_wallet/common/extensions/TimberExtension.kt
@@ -0,0 +1,36 @@
+package com.behzoddev.e_wallet.common.extensions
+
+import timber.log.Timber
+
+/**
+ * This extensions builds on Timber with an API that's easier to use from Kotlin.
+ * Instead of using formatting parameters, you pass a lambda that is only evaluated
+ * if the message is logged.
+ */
+
+/**
+ * Extension function that wraps Timber's debug logging
+ */
+inline fun debug(t: Throwable? = null,message: () -> String) {
+    return log { Timber.d(t,message())}
+}
+
+/**
+ * Extension function that wraps Timber's info logging
+ */
+inline fun info(message: () -> String) {
+    return log { Timber.i(message()) }
+}
+
+/**
+ * Extension function that wraps Timber's error logging
+ */
+inline fun error(t: Throwable? = null, message: () -> String) {
+    return log { Timber.e(t,message()) }
+}
+
+
+@PublishedApi
+internal inline fun log(block: () -> Unit) {
+    if (Timber.treeCount > 0) block()
+}
