b2bcdaad5ae07b8d730edf8309dac20fabfca978
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-19 16:46:05 +0900

    Update WalletFragment.kt

--- app/src/main/java/com/behzoddev/e_wallet/ui/wallet/WalletFragment.kt
+++ app/src/main/java/com/behzoddev/e_wallet/ui/wallet/WalletFragment.kt
@@ -4,19 +4,52 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.behzoddev.e_wallet.common.base.BaseFragment
+import com.behzoddev.e_wallet.common.extensions.toastShort
+import com.behzoddev.e_wallet.data.local.TransactionModel
 import com.behzoddev.e_wallet.databinding.FragmentWalletBinding
+import com.behzoddev.e_wallet.presentation.wallet.WalletViewModel
+import com.behzoddev.e_wallet.utils.states.TransactionViewState
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
+@AndroidEntryPoint
 class WalletFragment : BaseFragment<FragmentWalletBinding>() {
+
+    private lateinit var walletAdapter: WalletAdapter
+    private val walletViewModel: WalletViewModel by viewModels()
 
     override fun initializeBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentWalletBinding {
-        return FragmentWalletBinding.inflate(inflater,container,false)
+        return FragmentWalletBinding.inflate(inflater, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initializeRecyclerView()
     }
+
+    private fun initializeRecyclerView() = with(binding) {
+        walletAdapter = WalletAdapter()
+        val recyclerView = transactionRecyclerView
+        recyclerView.adapter = walletAdapter
+    }
+    private fun observerWallet() = lifecycleScope.launchWhenCreated {
+        walletViewModel.dashboardState.collect {
+            when(it) {
+                is TransactionViewState.Loading -> {}
+                is TransactionViewState.Success -> {
+                    getAllTransactions(it.data)
+                }
+                is TransactionViewState.Error -> {
+                    toastShort("Cannot load data")
+                }
+            }
+        }
+    }
+    private fun getAllTransactions(list: List<TransactionModel>) = walletAdapter.differ.submitList(list)
 }
