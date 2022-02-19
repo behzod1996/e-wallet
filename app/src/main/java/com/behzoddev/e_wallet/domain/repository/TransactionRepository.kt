1d5d6673d69f7dcce796895dac0d50b0f3b1b711
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-19 23:08:00 +0900

    Update repository

--- app/src/main/java/com/behzoddev/e_wallet/domain/repository/TransactionRepository.kt
+++ app/src/main/java/com/behzoddev/e_wallet/domain/repository/TransactionRepository.kt
@@ -4,9 +4,9 @@ import com.behzoddev.e_wallet.data.local.TransactionModel
 import kotlinx.coroutines.flow.Flow
 
 interface TransactionRepository {
-        suspend fun insertTransaction(transactionModel: TransactionModel): Long
-        suspend fun updateTransaction(transactionModel: TransactionModel): Int
-        suspend fun deleteTransaction(transactionModel: TransactionModel): Int
-        fun getAllTransactions(): Flow<List<TransactionModel>>
-        fun getById(transactionId: Int): Flow<TransactionModel>
+    suspend fun insertTransaction(transactionModel: TransactionModel): Long
+    suspend fun updateTransaction(transactionModel: TransactionModel): Int
+    suspend fun deleteTransaction(transactionModel: TransactionModel): Int
+    fun getAllTransactions(): Flow<List<TransactionModel>>
+    fun getById(transactionId: Int): Flow<TransactionModel>
 }
