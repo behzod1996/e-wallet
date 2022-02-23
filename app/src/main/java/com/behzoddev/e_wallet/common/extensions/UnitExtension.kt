0b162a17b7335800ee3ad5eb0c1abe8729639610
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-23 19:14:14 +0900

    Update UnitExtension.kt

--- app/src/main/java/com/behzoddev/e_wallet/common/extensions/UnitExtension.kt
+++ app/src/main/java/com/behzoddev/e_wallet/common/extensions/UnitExtension.kt
@@ -6,6 +6,6 @@ import java.util.*
 fun convertToString(amount: Double) : String {
     val format: NumberFormat = NumberFormat.getCurrencyInstance()
     format.maximumFractionDigits = 0
-    format.currency = Currency.getInstance("USD")
+    format.currency = Currency.getInstance("KRW")
     return format.format(amount)
 }
